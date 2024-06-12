7ac8baa95a0ad9790bf976e2b8abc5a9a4d35e5a
unknown <renanalencar02@gmail.com>
2024-06-12 11:59:51 -0300

    feat: criado o metodo de listagem de propostas da base

--- src/main/java/com/alencar/propostaapp/service/PropostaService.java
+++ src/main/java/com/alencar/propostaapp/service/PropostaService.java
@@ -9,17 +9,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.beans.Transient;
+import java.util.List;
 
 @Service
 public class PropostaService {
     @Autowired
     private PropostaRepository propostaRepository;
+    @Autowired
+    private PropostaMapper propostaMapper;
 
-    @Transient
     public PropostaResponseDto criar (PropostaRequestDto requestDto) {
-        Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(requestDto);
-        System.out.println(proposta.toString());
+        Proposta proposta = propostaMapper.convertDtoToProposta(requestDto);
         propostaRepository.save(proposta);
-        return PropostaMapper.INSTANCE.convertPropostaToDto(proposta);
+        return propostaMapper.convertPropostaToDto(proposta);
     }
+
+    public List<PropostaResponseDto> obterProposta() {
+        return propostaMapper.convertListEntityToListDto(propostaRepository.findAll());
+    }
+
 }
