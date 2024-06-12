fdfae44937c111d7d4856e33b0c254a845364fe3
unknown <renanalencar02@gmail.com>
2024-06-12 11:48:47 -0300

    feat: criada a URI de retorno do metodo de criar proposta ecriado o metodo get para listagem completa de propostas

--- src/main/java/com/alencar/propostaapp/controller/PropostaController.java
+++ src/main/java/com/alencar/propostaapp/controller/PropostaController.java
@@ -5,10 +5,10 @@ import com.alencar.propostaapp.dto.PropostaResponseDto;
 import com.alencar.propostaapp.service.PropostaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/proposta")
@@ -19,8 +19,16 @@ public class PropostaController {
 
     @PostMapping
     public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto){
-        System.out.println(requestDto.toString());
         PropostaResponseDto response = propostaService.criar(requestDto);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(response.getId()).toUri())
+                .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PropostaResponseDto>> obterPropostas(){
+        return ResponseEntity.ok(propostaService.obterProposta());
     }
 }
